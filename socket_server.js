21bd7f45d9ab2dd52f471c4d018d2ea7f063ea8c
Wang Yuanhang <yuanhangw.sg@gmail.com>
2013-02-14 20:54:50 +0800

    little'

--- socket_server.js
+++ socket_server.js
@@ -1,21 +1,12 @@
-var io = require('socket.io').listen(parseInt(process.env.PORT))
-  , i = 0
+var io = require('socket.io').listen(parseInt(process.env.PORT||5000));
 
-
-io.configure(function () {
-  io.set("origins", "*:*");
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
 });
 
-
 io.sockets.on('connection', function (socket) {
-	console.log(">>>>>>client connected through socket");
-  socket.emit('news', '>>>>>>server say hello to client', i);
-  console.log('>>>>>>server say hello to client' +'['+i+']')
-  socket.on('my other event', function (data) {
-  	socket.emit('news', i);
-  	i++;
-  	console.log(data +'['+i+']');
+  socket.on('disconnect', function () {
+    io.sockets.emit('user disconnected');
   });
 });
