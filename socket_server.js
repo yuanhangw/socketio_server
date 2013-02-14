c64242d53bf9d1d8e641d8a48360c53e143ea809
Wang Yuanhang <yuanhangw.sg@gmail.com>
2013-02-15 00:37:43 +0800

    finally

--- socket_server.js
+++ socket_server.js
@@ -1,5 +1,5 @@
 var io = require('socket.io').listen(parseInt(process.env.PORT||5000));
-
+var i = 0;
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
@@ -8,6 +8,8 @@ io.configure(function () {
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
+  	socket.emit('news', i);
     console.log(data);
+    i++;
   });
 });
