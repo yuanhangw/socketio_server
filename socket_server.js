e45fba192b427752e6c7b032c3ba7d43aa868735
Wang Yuanhang <yuanhangw.sg@gmail.com>
2013-02-15 12:33:15 +0800

    let's rock

--- socket_server.js
+++ socket_server.js
@@ -1,15 +1,25 @@
 var io = require('socket.io').listen(parseInt(process.env.PORT||5000));
 var i = 0;
+var usernames = {};
+
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-  	socket.emit('news', i);
-    console.log(data);
-    i++;
-  });
+
+	socket.on('add_user', function(user_name){
+		console.log('add user');
+		socket.name = user_name;
+		usernames[user_name] = user_name; 
+		io.sockets.emit('updateusers', usernames);
+	});
+  
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  // 	socket.emit('news', i);
+  //   console.log(data);
+  //   i++;
+  // });
 });
